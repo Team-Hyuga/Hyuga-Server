d751c9d1bc383402dc2f4823990f400efc3e7cee
Kangmin <ehfehf0629@gmail.com>
2023-06-12 02:00:10 +0900

    feat: FilterConfig

---
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/global/filter/FilterConfig.kt
@@ -0,0 +1,15 @@
+package team.hyuga.server.global.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+
+class FilterConfig(
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        // TODO
+    }
+}
