772bc3bc2a8708bde78e34c6634bea0d22b78293
Kangmin <ehfehf0629@gmail.com>
2023-06-12 02:01:16 +0900

    feat: ExceptionHandlerFilter

---
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/global/filter/ExceptionHandlerFilter.kt
@@ -0,0 +1,51 @@
+package team.hyuga.server.global.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import team.hyuga.server.common.error.BusinessException
+import team.hyuga.server.global.error.ErrorResponse
+import team.hyuga.server.global.exception.InternalServerErrorException
+import java.nio.charset.StandardCharsets
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class ExceptionHandlerFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (exception: Exception) {
+            when(exception) {
+                is BusinessException -> setErrorResponse(response, exception)
+                else -> {
+                    exception.printStackTrace()
+                    setErrorResponse(response, InternalServerErrorException)
+                }
+            }
+        }
+    }
+
+    private fun setErrorResponse(
+        response: HttpServletResponse,
+        exception: BusinessException
+    ) {
+        response.apply {
+            characterEncoding = StandardCharsets.UTF_8.name()
+            status = exception.errorProperty.status()
+            contentType = MediaType.APPLICATION_JSON_VALUE
+            writer.write(
+                objectMapper.writeValueAsString(ErrorResponse.of(exception))
+            )
+        }
+    }
+}
