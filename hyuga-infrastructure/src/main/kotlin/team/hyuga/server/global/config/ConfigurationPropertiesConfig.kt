8d118a84dcf2ef7777c9134175c504434af125d1
Kangmin <ehfehf0629@gmail.com>
2023-06-16 23:41:53 +0900

    feat: ConfigurationPropertiesConfig

---
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/global/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package team.hyuga.server.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan("team.hyuga.server")
+@Configuration
+class ConfigurationPropertiesConfig
