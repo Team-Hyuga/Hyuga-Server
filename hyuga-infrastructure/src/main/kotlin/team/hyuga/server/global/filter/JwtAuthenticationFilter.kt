7e190260e1554e411c936799f348c0f61db82152
Kangmin <ehfehf0629@gmail.com>
2023-06-16 23:48:33 +0900

    feat: JwtAuthenticationFilter

---
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/global/filter/JwtAuthenticationFilter.kt
@@ -0,0 +1,35 @@
+package team.hyuga.server.global.filter
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import team.hyuga.server.global.security.jwt.JwtConstant
+import team.hyuga.server.global.security.jwt.JwtParser
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtAuthenticationFilter(
+    private val jwtParser: JwtParser
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = getToken(request)
+        token?.let {
+            SecurityContextHolder.getContext().authentication = jwtParser.getAuthentication(token)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun getToken(request: HttpServletRequest): String? {
+        val token = request.getHeader(JwtConstant.HEADER)
+
+        return if (!token.isNullOrEmpty() && token.startsWith(JwtConstant.PREFIX)) {
+            token.substring(JwtConstant.PREFIX.length);
+        } else null;
+    }
+}
