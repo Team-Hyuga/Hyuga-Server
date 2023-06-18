fb1cc11674384ee56e7f8dbb054fc22cb95b1d4a
Kangmin <ehfehf0629@gmail.com>
2023-06-18 16:49:28 +0900

    feat: TokenResponse

---
+++ hyuga-core/src/main/kotlin/team/hyuga/server/domain/auth/dto/TokenResponse.kt
@@ -0,0 +1,10 @@
+package team.hyuga.server.domain.auth.dto
+
+import java.time.LocalDateTime
+
+data class TokenResponse(
+    val accessToken: String,
+    val accessTokenExpiredAt: LocalDateTime,
+    val refreshToken: String,
+    val refreshTokenExpiredAt: LocalDateTime,
+)
