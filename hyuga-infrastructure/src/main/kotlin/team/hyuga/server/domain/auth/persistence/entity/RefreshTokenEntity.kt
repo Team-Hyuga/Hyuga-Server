c1bbe366027ac04abc8ef95a0c83fc8ccda3fd30
Kangmin <ehfehf0629@gmail.com>
2023-06-18 14:21:14 +0900

    feat: RefreshTokenEntity

---
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/domain/auth/persistence/entity/RefreshTokenEntity.kt
@@ -0,0 +1,21 @@
+package team.hyuga.server.domain.auth.persistence.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+import team.hyuga.server.domain.auth.model.AuthType
+
+@RedisHash
+class RefreshTokenEntity(
+        @Id
+    val token: String,
+
+        @Indexed
+    val email: String,
+
+        val authType: AuthType,
+
+        @TimeToLive
+    val ttl: Int
+)
