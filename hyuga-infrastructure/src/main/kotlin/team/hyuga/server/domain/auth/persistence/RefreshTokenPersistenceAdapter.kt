039cf5f8d531141c14865ae164951ffb02fe8680
Kangmin <ehfehf0629@gmail.com>
2023-06-18 16:15:21 +0900

    chore: Override RefreshTokenPort

--- hyuga-infrastructure/src/main/kotlin/team/hyuga/server/domain/auth/persistence/RefreshTokenPersistenceAdapter.kt
+++ hyuga-infrastructure/src/main/kotlin/team/hyuga/server/domain/auth/persistence/RefreshTokenPersistenceAdapter.kt
@@ -1,11 +1,26 @@
 package team.hyuga.server.domain.auth.persistence
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import team.hyuga.server.domain.auth.mapper.RefreshTokenMapper
+import team.hyuga.server.domain.auth.model.RefreshToken
 import team.hyuga.server.domain.auth.persistence.repository.RefreshTokenJpaRepository
+import team.hyuga.server.domain.auth.spi.RefreshTokenPort
 
 @Component
 class RefreshTokenPersistenceAdapter(
-    private val refreshTokenJpaRepository: RefreshTokenJpaRepository
-) {
-    
+    private val refreshTokenJpaRepository: RefreshTokenJpaRepository,
+    private val refreshTokenMapper: RefreshTokenMapper
+) : RefreshTokenPort {
+
+    override fun saveRefreshToken(refreshToken: RefreshToken) =
+        refreshTokenMapper.toDomain(
+            refreshTokenJpaRepository.save(
+                refreshTokenMapper.toEntity(refreshToken)
+            )
+        )!!
+
+    override fun queryByToken(token: String) = refreshTokenMapper.toDomain(
+        refreshTokenJpaRepository.findByIdOrNull(token)
+    )
 }
